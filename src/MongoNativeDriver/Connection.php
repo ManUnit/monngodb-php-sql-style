857e51cf37e11fa2b914295d39816391fa329198
manunit <hs1gab@gmail.com>
2020-07-01 21:57:13 +0700

    Update 05

--- src/MongoNativeDriver/Connection.php
+++ src/MongoNativeDriver/Connection.php
@@ -153,7 +153,6 @@ class Connection {  //defind the Class to be  master class
          $config->setDb((new static)->getDbNonstatic()) ;
          $conclude = new BuildConnect ; 
          $reactionInsert = $conclude->insertDoc($config ,(new static)->getCollectNonstatic() , $arrVals ) ; 
-        // dd ( $reactionInsert ) ;
          return  [ 1 ,$reactionInsert ] ; 
      }
 
